6152da7e25d38356f6345dbaff594a94d3973706
Victor <victor.brezoev@gmail.com>
2026-08-03 12:55:45 +0300

    Initialize dbt project and add customers staging model

---
+++ dbt/models/staging/stg_customers.sql
@@ -0,0 +1,11 @@
+with customers as (
+    select * from {{source ("raw", "olist_customers_dataset") }}
+)
+
+select
+    customer_id as id,
+    customer_unique_id as unique_id,
+    customer_zip_code_prefix as zip_code,
+    customer_city as city,
+    customer_state as state
+from customers
