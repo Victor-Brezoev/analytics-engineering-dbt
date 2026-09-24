2f67d01533b0b584dac61997a4656ef7e35350e4
Victor <victor.brezoev@gmail.com>
2026-09-24 15:05:46 +0300

    Add order items fact table

---
+++ dbt/models/marts/fct_order_items.sql
@@ -0,0 +1,19 @@
+with order_items as (
+    select * from {{ ref('int_order_items_enriched') }}
+),
+
+final as (
+    select
+        order_id,
+        item_id,
+        product_id,
+        seller_id,
+        category_name,
+        price,
+        freight_value,
+        volume_cm3,
+        price + freight_value as item_total_value
+        from order_items
+)
+
+select * from final
--- dbt/tests/int_order_items_unique.sql
+++ dbt/tests/int_order_items_unique.sql
@@ -1,4 +1,9 @@
-select order_id, item_id, count(*) as row_count
+select 
+order_id, 
+item_id, 
+count(*) as row_count
 from {{ ref('int_order_items_enriched')}}
-group by order_id, item_id
+group by
+ order_id, 
+ item_id
 having count(*) > 1
